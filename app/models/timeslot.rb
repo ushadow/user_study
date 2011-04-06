e5636c6dd6c91234c7885e591d43427823793f5f
yingyin <clevershadow@gmail.com>
2011-04-06 01:19:11 -0400

    Changed background color.

--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -1,6 +1,10 @@
 class Timeslot < ActiveRecord::Base
   has_many :signups
 
+  def self.open_timeslots
+    Timeslot.all.select { |t| t.openings_left > 0 }.map { |t| [t.name, t.id] }
+  end
+
   def openings_left
     openings - signups.length    
   end
